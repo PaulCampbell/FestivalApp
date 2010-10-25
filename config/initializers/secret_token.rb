1d60e26aeb60088f65c531836bc2bede6d28d97c
paulcampbell <paulgcampbell@gmail.com>
2010-10-25 19:18:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FestivalApp::Application.config.secret_token = 'cfa31139c59cd3365d054f6ec3eca3881a16052bc44db315cbe84df5d76b6ba1bd9ae1b688889583969c5aeaa2a326124c8bc672e61ce97aa5c7f82c740beead'
